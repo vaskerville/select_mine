98e8b6e8595472caf1ac8340e9101a2f659cf6e5
vaskerville <tradertaka+github@gmail.com>
2017-12-18 17:16:19 +0900

    client.js

--- client.js
+++ client.js
@@ -10,71 +10,71 @@ const errorfile = './error.log';
 const killznyd = './yenten'
 const killytnd = './bitzeny'
 let now = 0;
+let stop = 0;
 
 (async() => {
-  try {
-    while (true) {
-      // 文字列取得
-      let url = 'http://www3.eltrans.jp/select_this';
-      let response = await fetch(url);
-      let json = await response.text();
-      console.log(json);
+    try {
+      while (true) {
+        // 文字列取得
+        let url = 'http://www3.eltrans.jp/select_this';
+        let response = await fetch(url);
+        let json = await response.text();
+        console.log(json);
 
-      if (json == 1 && now != 1) {
-        //znyに切り替えろ
-        exec(killytnd, (err, stdout, stderr) => {
-          if (err) {
-            console.log(err);
-          }
-          console.log(stdout);
-        });
-        now = 1;
-      } else {
-        if (json == 2 && now != 2) {
-          //ytnに切り替えろ
-          exec(killznyd, (err, stdout, stderr) => {
-            if (err) {
-              console.log(err);
+        if (json == 1 && (now != 1 || stop == 1) { // go znyかつ、今は2もしくはstopフラグあり
+            //znyに切り替えろ
+            exec(killytnd, (err, stdout, stderr) => {
+              if (err) {
+                console.log(err);
+              }
+              console.log(stdout);
+            });
+            now = 1;
+            stop = 0;
+          } else {
+            if (json == 2 && (now != 2 || stop == 1)) { //　
+              //ytnに切り替えろ
+              exec(killznyd, (err, stdout, stderr) => {
+                if (err) {
+                  console.log(err);
+                }
+                console.log(stdout);
+              });
+              now = 2;
+              stop = 0;
             }
-            console.log(stdout);
+          }
+
+          psList().then(data => {
+            let newLine = data.filter(function (item, index) {
+              if (item.name == 'cpuminer' || item.name == 'minerd') return true;
+            });
+            console.log(newLine);
+            if (Object.keys(newLine).length === 0) {
+              stop = 1;
+              let date = new Date();
+              let formattedDate = date.toFormat("YYYY/MM/DD HH24:MI:SS")
+              let errortowtite = formattedDate + "vanished";
+              appendFile(errorfile, errortowrite);
+            }
           });
-          now = 2;
+
+          await sleep(1000 * 60);
+
         }
+      } catch (error) {
+        let date = new Date();
+        let formattedDate = date.toFormat("YYYY/MM/DD HH24:MI:SS")
+        let errortowtite = formattedDate + error;
+        appendFile(errorfile, errortowrite);
+        console.log(error);
       }
+    })();
 
-      psList().then(data => {
-        let newLine = data.filter(function (item, index) {
-          if (item.name == 'cpuminer' || item.name == 'minerd') return true;
-        });
-        console.log(newLine);
-        if (Object.keys(newLine).length === 0){
-          now = 0;
-          let date = new Date();
-          let formattedDate = date.toFormat("YYYY/MM/DD HH24:MI:SS")
-          let errortowtite = formattedDate + "vanished";
-          appendFile(errorfile, errortowrite);
-        }
-      });
-
-      await sleep(1000 * 60);
-
-    }
-  } catch (error) {
-    let date = new Date();
-    let formattedDate = date.toFormat("YYYY/MM/DD HH24:MI:SS")
-    let errortowtite = formattedDate + error;
-    appendFile(errorfile, errortowrite);
-    console.log(error);
+  function appendFile(path, data) {
+    fs.appendFile(path, data, function (err) {
+      if (err) {
+        throw err;
+      }
+    });
   }
-})();
-
-function appendFile(path, data) {
-  fs.appendFile(path, data, function (err) {
-    if (err) {
-      throw err;
-    }
-  });
-}
-
-
-
